8bef58aa0f937ec2c31a11e318a4246163e29154
Keturah Walters <your.ketwalters@gmail.com>
2014-02-09 23:46:21 -0500

    add emergency compliments and citibike app

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'haml'
+gem 'sass'
+gem 'compass'
 
 group :development do
   gem "shotgun"
--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'bundler'
 Bundler.require
 
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+
+  set :haml, { :format => :html5 }
+  set :sass, Compass.sass_engine_options
+  set :scss, Compass.sass_engine_options
+end
 
 class Portfolio < Sinatra::Application
 
